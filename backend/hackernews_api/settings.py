21f41b4d69c410142ce9e8b22b7aa1e8311eacf9
Adesina Joshua <tolujosh1@gmail.com>
2024-01-29 08:52:49 +0100

    feature:postgresSQL setup

--- backend/hackernews_api/settings.py
+++ backend/hackernews_api/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = "hackernews_api.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Hackernews',
+        'USER': 'admin',
+        'PASSWORD': 'joshmanfood',
+        'HOST': 'localhost',  # Assuming your PostgreSQL server is running locally
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
